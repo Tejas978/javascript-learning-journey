992a1316aabc1b2e82b9139aac254b9662f88361
Tejas978git config --global user.name Tejas978 <khairetejas76@gmail.com>
2025-12-24 23:17:00 +0530

    Basics of JS

---
+++ 1_Variables.js
@@ -0,0 +1,14 @@
+// console.log("Hello Workd");
+const nameID = "Tejas Khaire"
+console.log(nameID);
+//
+let city = "Mumbai"
+city = "Chennai"
+console.log(city);
+city = "Pune"
+console.log(city)
+let country;  //For Undefined
+//
+// State;
+
+console.table([nameID, city, city, country])

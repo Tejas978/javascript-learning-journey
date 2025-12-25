badaadf9330a2d451ab26fc0b3fd680e5b8f8bf9
Tejas978git config --global user.name Tejas978 <khairetejas76@gmail.com>
2025-12-25 23:12:03 +0530

    JS String Concepts

---
+++ 8_String.js
@@ -0,0 +1,36 @@
+// let names = "Tejas"
+// console.log(names);
+//
+
+// let acc = "Tejas Khaire"
+// let age = 21
+
+// console.log(`Hello everyone myself ${acc} and I am ${age} years old`);
+
+const acc_name = "Tejas-Khaire"
+console.log(acc_name[0]); // Outputs: T
+console.log(acc_name.__proto__);
+
+// console.log(acc_name.length)
+// console.log(acc_name.toUpperCase())
+console.log(acc_name.charAt(2))
+console.log(acc_name.indexOf("j"));
+//
+
+//
+
+// console.log(acc_name.substring(0,6));
+// console.log(acc_name.slice(0,6));
+
+// const newStringOne = "   hitesh    "
+// console.log(newStringOne);
+// console.log(newStringOne.trim());
+
+// const url = "https://hitesh.com/hitesh%20choudhary"
+
+// console.log(url.replace('%20', '-'))
+
+// console.log(url.includes('sundar'))
+
+// console.log(gameName.split('-'));
+

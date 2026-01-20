9d1221490b3d01923d068bd6daca8c589e5e0c9a
Tejas978git config --global user.name Tejas978 <khairetejas76@gmail.com>
2026-01-20 23:43:17 +0530

    JS DOM Learing

---
+++ 31_DOM.js
@@ -0,0 +1,21 @@
+// let head = document.getElementById("myid")
+// console.log(head)
+// let moreclass = document.getElementsByClassName("heading")
+// console.log(moreclass)
+
+//let parra = document.getElementsByTagName("p")
+//console.log(parra)
+
+// let SEel = document.querySelector("p")
+// console.log(SEel)
+
+// let SEels = document.querySelectorAll("p")
+// console.log(SEels)
+
+// let taggname = document.querySelector("p")
+// console.log(taggname)
+// let tagger = document.querySelector("div")
+// console.log(tagger)
+
+const myele = document.querySelector("div")
+console.log(myele.firstElementChild)

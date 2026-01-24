f518a5f86230e3c8a97437da302ca8f0703421c5
Tejas978git config --global user.name Tejas978 <khairetejas76@gmail.com>
2026-01-24 22:39:06 +0530

    js Dom

---
+++ 32_DOM.js
@@ -0,0 +1,12 @@
+let div = document.querySelector("div")
+// console.log(div)
+
+// let hello = div.getAttribute("id")
+// console.log(hello)
+
+// let hello = div.setAttribute("id","newmatch")
+// console.log(hello)
+
+// div.style.color = "red";
+// div.style.color = "yellow";
+

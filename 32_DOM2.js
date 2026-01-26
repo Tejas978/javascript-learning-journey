5142a2721ea10ea0cb6f68820195d5483871479b
Tejas978git config --global user.name Tejas978 <khairetejas76@gmail.com>
2026-01-26 20:02:27 +0530

    js Dom

---
+++ 32_DOM2.js
@@ -0,0 +1,23 @@
+let newitem  =  document.createElement("button")  //to create a element first...
+newitem.innerText = "Subscribe"
+console.log(newitem)
+
+//inside the TAGS
+
+// let hello = document.querySelector("div")
+// hello.append(newitem)
+
+// let hello = document.querySelector("div")
+// hello.prepend(newitem)  
+
+//for TAGS respectively,OUTSIDE THE TAGS
+
+// let hello = document.querySelector("div")
+// hello.before(newitem)
+
+// let hello = document.querySelector("div")
+// hello.after(newitem)
+
+newitem.remove()
+
+
